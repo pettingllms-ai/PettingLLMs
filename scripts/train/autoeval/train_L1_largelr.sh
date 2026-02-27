a52dd0b0c7df94a99dea547e5b86f55a6c9c1437
Mercury7353 <zhangyaolun5@gmail.com>
2026-02-28 04:51:33 +0800

    smallgpuusage

--- scripts/train/autoeval/train_L1_largelr.sh
+++ scripts/train/autoeval/train_L1_largelr.sh
@@ -80,7 +80,7 @@ python -m pettingllms.trainer.train --config-path ../config/autoevol --config-na
     +$model_0_config_path.actor.use_kl_loss=false\
     +$model_0_config_path.actor.kl_loss_coef=0.0\
     +$model_0_config_path.actor.entropy_coeff=0.00\
-    $model_0_config_path.actor_rollout_ref.rollout.gpu_memory_utilization=0.8\
+    $model_0_config_path.actor_rollout_ref.rollout.gpu_memory_utilization=0.6\
 
 
 # +$model_0_config_path.actor.clip_ratio_low=0.15\
