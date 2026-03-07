6bcf69050c531d450c54546dc6fd1a966664f737
Mercury7353 <zhangyaolun5@gmail.com>
2026-03-07 08:29:04 +0800

    trainstage2

--- scripts/train/autoeval/train_withstage.sh
+++ scripts/train/autoeval/train_withstage.sh
@@ -62,11 +62,11 @@ model_0_resource="resource.n_gpus_per_node=$GPU_num  $model_0_config_path.traine
 # /mnt/afs/zhangyaolun/safe_model/tool/PettingLLMs/checkpoints/autoeval_mixcoldstart_8design_1execution_designonly_8gpus/global_step_20/actor/checkpoint
 python -m pettingllms.trainer.train --config-path ../config/autoevol --config-name math_L1_prompt \
     $model_0_resource \
-    base_models.policy_0.path="Mercury7353/masrl_0228_mix_coldstart"\
+    base_models.policy_0.path="[lastckptpath]"\
     lora_rank=0\
     lora_alpha=16\
     training.experiment_name=autoeval_mixcoldstart_4design_8execution_5e_6_trainall_withstage\
-    training.total_training_steps=100\
+    training.total_training_steps=200\
     training.train_batch_size=8\
     training.design_sample_num=8\
     training.execute_sample_num=4\
@@ -75,7 +75,7 @@ python -m pettingllms.trainer.train --config-path ../config/autoevol --config-na
     training.max_response_length=8192\
     training.val_freq=10\
     training.save_freq=10\
-    training.train_data_mode=designer_only\
+    training.train_data_mode=all\
     env.dataset=polaris\
     'env.benchmark=[AIME24,AIME25]'\
     $model_0_config_path.trainer.val_before_train=True\
@@ -83,7 +83,7 @@ python -m pettingllms.trainer.train --config-path ../config/autoevol --config-na
     $model_0_config_path.actor.ppo_micro_batch_size_per_gpu=1\
     $model_0_config_path.actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2\
     $model_0_config_path.actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=true\
-    +$model_0_config_path.actor.optim.lr=5e-6\
+    +$model_0_config_path.actor.optim.lr=3e-6\
     +$model_0_config_path.actor.use_kl_loss=false\
     +$model_0_config_path.actor.kl_loss_coef=0.0\
     +$model_0_config_path.actor.entropy_coeff=0.00\
