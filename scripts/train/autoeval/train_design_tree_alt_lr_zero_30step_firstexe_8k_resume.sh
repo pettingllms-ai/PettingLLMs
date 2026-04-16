29d4dcf8c76cb12b699e84f061ca76661e32c6b3
Mercury7353 <zhangyaolun5@gmail.com>
2026-04-17 07:43:16 +0800

    6k

--- scripts/train/autoeval/train_design_tree_alt_lr_zero_30step_firstexe_8k_resume.sh
+++ scripts/train/autoeval/train_design_tree_alt_lr_zero_30step_firstexe_8k_resume.sh
@@ -104,7 +104,7 @@ TRAIN_BATCH_SIZE=${TRAIN_BATCH_SIZE:-8}
 EXECUTOR_GROUP_MODE=${EXECUTOR_GROUP_MODE:-question}
 MODEL_PATH=${MODEL_PATH:-"Mercury7353/masrl_0228_mix_coldstart"}
 APPS_RATIO=${APPS_RATIO:-0.7}
-EXPERIMENT_NAME=${EXPERIMENT_NAME:-"autoeval_mix_${DESIGN_SAMPLE_NUM}d_${EXECUTE_SAMPLE_NUM}e_mix_question_grouping_altlr_zerolr_30steps_firstexe_6k"}
+EXPERIMENT_NAME=${EXPERIMENT_NAME:-"autoeval_mix_4d_4e_mix_question_grouping_altlr_zerolr_30steps_firstexe"}
 
 python -m pettingllms.trainer.train --config-path ../config/autoevol --config-name math_L1_prompt \
     $model_0_resource \
@@ -120,8 +120,8 @@ python -m pettingllms.trainer.train --config-path ../config/autoevol --config-na
     training.validate_sample_num=1\
     training.max_prompt_length=6000\
     training.max_response_length=8192\
-    training.val_freq=10\
-    training.save_freq=10\
+    training.val_freq=100\
+    training.save_freq=5\
     training.train_data_mode=all\
     training.designer_lr=1e-9\
     training.executor_lr=5e-6\
